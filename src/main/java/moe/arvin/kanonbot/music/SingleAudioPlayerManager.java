218cbd5cb0bc2c1c513fe2402539acc0053fcb67
Arvin Ziaei <arvinz2k@gmail.com>
2024-05-23 23:43:23 -0700

    update dependencies, bump version

--- src/main/java/moe/arvin/kanonbot/music/SingleAudioPlayerManager.java
+++ src/main/java/moe/arvin/kanonbot/music/SingleAudioPlayerManager.java
@@ -4,6 +4,11 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
+import dev.lavalink.youtube.YoutubeAudioSourceManager;
+import dev.lavalink.youtube.clients.AndroidWithThumbnail;
+import dev.lavalink.youtube.clients.MusicWithThumbnail;
+import dev.lavalink.youtube.clients.WebWithThumbnail;
+import dev.lavalink.youtube.clients.skeleton.Client;
 
 public final class SingleAudioPlayerManager {
 
@@ -13,8 +18,13 @@ public final class SingleAudioPlayerManager {
         PLAYER_MANAGER = new DefaultAudioPlayerManager();
         PLAYER_MANAGER.getConfiguration().setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
         PLAYER_MANAGER.getConfiguration().setFilterHotSwapEnabled(true);
-        AudioSourceManagers.registerRemoteSources(PLAYER_MANAGER);
+        AudioSourceManagers.registerRemoteSources(PLAYER_MANAGER, com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager.class);
         AudioSourceManagers.registerLocalSource(PLAYER_MANAGER);
+        YoutubeAudioSourceManager youtube = new YoutubeAudioSourceManager(
+                true,
+                new Client[] { new MusicWithThumbnail(), new WebWithThumbnail(), new AndroidWithThumbnail() }
+        );
+        PLAYER_MANAGER.registerSourceManager(youtube);
     }
 
     public static AudioPlayerManager getInstance() {
